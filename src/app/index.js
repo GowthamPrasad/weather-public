688a507f5a53a22b750fb96bc3f75078e30fe653
Gowtham Prasad <50653813+GowthamPrasad@users.noreply.github.com>
2023-01-12 07:12:33 +0530

    Update index.js

--- src/app/index.js
+++ src/app/index.js
@@ -5,8 +5,8 @@
 //   url: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
 //   params: {city: 'Seattle'},
 //   headers: {
-//     'X-RapidAPI-Key': '08ae8b4222msh06098cc16e267dbp1bd4a7jsne6fa1086f454',
-//     'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+//     'X-RapidAPI-Key': '',
+//     'X-RapidAPI-Host': ''
 //   }
 // };
 
@@ -24,12 +24,12 @@
 //   url: 'https://openweather43.p.rapidapi.com/weather',
 //   params: {
 //     q: 'Dubai',
-//     appid: ['da0f9c8d90bde7e619c3ec47766a42f4', 'da0f9c8d90bde7e619c3ec47766a42f4'],
+//     appid: ['', ''],
 //     units: 'standard'
 //   },
 //   headers: {
-//     'X-RapidAPI-Key': '08ae8b4222msh06098cc16e267dbp1bd4a7jsne6fa1086f454',
-//     'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+//     'X-RapidAPI-Key': '',
+//     'X-RapidAPI-Host': ''
 //   }
 // };
 
@@ -47,8 +47,8 @@
 //   url: 'https://foreca-weather.p.rapidapi.com/location/search/mumbai',
 //   qs: {lang: 'en', country: 'in'},
 //   headers: {
-//     'X-RapidAPI-Key': '08ae8b4222msh06098cc16e267dbp1bd4a7jsne6fa1086f454',
-//     'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com',
+//     'X-RapidAPI-Key': '',
+//     'X-RapidAPI-Host': '',
 //     useQueryString: true
 //   }
 // };
@@ -69,8 +69,8 @@ const options = {
   url: 'https://foreca-weather.p.rapidapi.com/forecast/daily/102643743',
   qs: {alt: '0', tempunit: 'C', windunit: 'MS', periods: '8', dataset: 'full'},
   headers: {
-    'X-RapidAPI-Key': '08ae8b4222msh06098cc16e267dbp1bd4a7jsne6fa1086f454',
-    'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com',
+    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Host': '',
     useQueryString: true
   }
 };
